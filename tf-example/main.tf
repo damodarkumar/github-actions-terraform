af0600e10fa509439cf1410f8cec3f988e4167a7
damodarkumar <dhamu1010@gmail.com>
2023-09-11 15:12:37 +0200

    adding subnet and vpc

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -21,9 +21,10 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
+  vpc_id                 = "vpc-0a5e26f5c2cf5a8d2"
   instance_type          = "t3.micro"
   key_name               = "githubworkflow-ec2-key"
-  vpc_id                 = "vpc-0a5e26f5c2cf5a8d2"
+  
 
   tags = {
     Name = var.ec2_name
