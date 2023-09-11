2320ddbe942f2e55afb0110cef5583f9a37a261b
damodarkumar <dhamu1010@gmail.com>
2023-09-11 14:33:35 +0200

    key added

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "backstage-instance-key"
+  key_name      = "githubworkflow-ec2-key"
 
   tags = {
     Name = var.ec2_name
