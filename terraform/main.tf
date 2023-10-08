dcfe8ef1a54abc5f9093b277fbe1bb33b2c219a2
damodarkumar <dhamu1010@gmail.com>
2023-10-08 09:43:48 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "pe_custom_vpc" {
 resource "aws_subnet" "pe_public_subnet" {
   vpc_id            = aws_vpc.pe_custom_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "1a"
+ # availability_zone = "1a"
 
   tags = {
     Name = "pe Public Subnet"
@@ -30,7 +30,7 @@ resource "aws_subnet" "pe_public_subnet" {
 resource "aws_subnet" "pe_private_subnet" {
   vpc_id            = aws_vpc.pe_custom_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "1a"
+ # availability_zone = "1a"
 
   tags = {
     Name = "pe Private Subnet"
