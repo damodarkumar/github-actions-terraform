0b0925225ca13fe78823ca9e5e7bfcab33697807
DAMODAR KUMAR P <49914008+dhamu1010@users.noreply.github.com>
2023-10-13 18:43:58 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,7 +119,7 @@ data "aws_ami" "ubuntu" {
 #Resource: aws_instance
 resource "aws_instance" "web_instance" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = t3.micro
+  instance_type = "t3.micro"
   key_name      = "githubworkflow-ec2-key"
 
   subnet_id                   = aws_subnet.pe_public_subnet.id
