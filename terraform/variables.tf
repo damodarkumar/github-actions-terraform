9904979fca6e12940e6a8e6cc511d54c3fb755fa
DAMODAR KUMAR P <49914008+dhamu1010@users.noreply.github.com>
2023-10-14 08:47:05 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,10 @@ variable "ec2_name" {
   type = string
 }
 
-variable "ec2_type" {
-  type = string
-}
+#variable "ec2_type" {
+#  type = string
+#}
 
-variable "provider_region" {
-  type = string
-}
+#variable "provider_region" {
+#  type = string
+#}
